3dfe7101fd6d45df4a5604cc6f9e8ec4859e29c2
Agung Dirgantara <agungmasda29@gmail.com>
2018-11-01 16:22:08 +0700

    add app helpers

---
+++ application/helpers/app_helper.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/* Site Language */
+function site_language($key=null,$default=null)
+{
+    return $default;
+}
+
+/* End of file app_helper.php */
+/* Location: ./application/helpers/app_helper.php */
