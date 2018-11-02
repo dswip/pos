08683e55bbe8f2c9673c77b3664305d693c2f840
Agung Dirgantara <agungmasda29@gmail.com>
2018-11-02 13:46:54 +0700

    add function return_if_exsists

--- application/helpers/app_helper.php
+++ application/helpers/app_helper.php
@@ -6,5 +6,45 @@ function site_language($key=null,$default=null)
     return $default;
 }
 
+
+/* Return If Exsist */
+function return_if_exists($data,$key=null,$onEmpty=null)
+{
+    switch (gettype($data))
+    {
+        case 'array':
+            if(!empty($key))
+            {
+                if(isset($data[$key]))
+                {
+                    return $data[$key];
+                }
+                else
+                {
+                    return (!empty($onEmpty))?$onEmpty:null;
+                }
+            }
+        break;
+
+        case 'object':
+            if(!empty($key))
+            {
+                if(isset($data->{$key}))
+                {
+                    return $data->{$key};
+                }
+                else
+                {
+                    return (!empty($onEmpty))?$onEmpty:null;
+                }
+            }
+        break;
+        
+        default:
+            return (!empty($data))?$data:(!empty($onEmpty))?$onEmpty:null;
+        break;
+    }
+}
+
 /* End of file app_helper.php */
 /* Location: ./application/helpers/app_helper.php */
