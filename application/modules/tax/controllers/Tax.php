eccc6ed7f1ad235e3339369faaa4c52a4cacf0d4
Agung Dirgantara <agungmasda29@gmail.com>
2018-11-03 14:59:38 +0700

    update gitignore

---
+++ application/modules/tax/controllers/Tax.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+class Tax extends Rest_api
+{
+	
+}
+
+/* End of file Tax.php */
+/* Location: ./application/modules/tax/controllers/Tax.php */
