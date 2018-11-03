1bec7e873b4d55580c8d15eed7089e9885eb086f
Agung Dirgantara <agungmasda29@gmail.com>
2018-11-03 15:00:10 +0700

    tax module

--- application/modules/tax/controllers/Tax.php
+++ application/modules/tax/controllers/Tax.php
@@ -1,7 +1,54 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 class Tax extends Rest_api
 {
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model([
+		]);
+	}
 	
+	/* List */
+	public function index_get()
+	{
+		
+	}
+
+	/* Add */
+	public function add_post()
+	{
+		
+	}
+
+	/* View */
+	public function view_get()
+	{
+		
+	}
+
+	/* Update */
+	public function update_post()
+	{
+		
+	}
+
+	/* Delete */
+	public function delete_post()
+	{
+		
+	}
+
+	/* Restore */
+	public function restore_post()
+	{
+		
+	}
+
+	/* Force Delete */
+	public function force_delete_post()
+	{
+		
+	}
 }
 
 /* End of file Tax.php */
