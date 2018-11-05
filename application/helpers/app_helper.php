4934e3fd2df386c855c4ae032074a7bc56096686
Agung Dirgantara <agungmasda29@gmail.com>
2018-11-05 16:26:42 +0700

    add function datatable query

--- application/helpers/app_helper.php
+++ application/helpers/app_helper.php
@@ -46,5 +46,81 @@ function return_if_exists($data,$key=null,$onEmpty=null)
     }
 }
 
+
+function datatable_query($model,$query)
+{
+    $search = (isset($query['search']['value']))?$query['search']['value']:FALSE;
+    $_model = $model;
+    (isset($query['in_trash']))? $_model = $_model->onlyTrashed():FALSE;
+    if(!empty($search))
+    {
+        foreach (show_columns($model->getTable()) as $column_key => $column)
+        {
+            if($column_key == 0)
+            {
+                $_model = $_model->where($column,'LIKE','%'.$search.'%');
+            }
+            else
+            {
+                $_model = $_model->orWhere($column,'LIKE','%'.$search.'%');
+            }
+        }
+    }
+
+    if(isset($query['columns']) && is_array($query['columns']))
+    {
+        foreach ($query['columns'] as $key => $column)
+        {
+            if(isset($query['columns'][$key]['search']['value']) && !empty($query['columns'][$key]['search']['value']))
+            {
+                $search_column = $query['columns'][$key]['data'];
+                $search_value  = $query['columns'][$key]['search']['value'];
+                $_model = $_model->where($search_column,'LIKE','%'.$search_value.'%');
+            }
+        }
+    }
+    
+    if(!empty($query['order']))
+    {
+        if(isset($query['columns']))
+        {
+            $column = $query['columns'][$query['order'][0]['column']]['data'];
+        }
+        else
+        {
+            if(isset($query['order']['column']))
+            {
+                $column = $query['order']['column'];
+            }
+            else
+            {
+                $column = 'id';
+            }
+        }
+
+        if(isset($query['order'][0]['dir']))
+        {
+            $type = $query['order'][0]['dir'];
+        }
+        else
+        {
+            if(isset($query['order']['type']))
+            {
+                $type = $query['order']['type'];    
+            }
+            else
+            {
+                $type = 'asc';
+            }
+        }
+
+        if(in_array($column, show_columns($model->getTable())))
+        {
+            $_model = $_model->orderBy($column,$type);
+        }
+    }
+    return $_model;
+}
+
 /* End of file app_helper.php */
 /* Location: ./application/helpers/app_helper.php */
