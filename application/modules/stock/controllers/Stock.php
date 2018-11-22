dc0aaf63a0a96552f3d41cab88f9171af84e3cd0
Agung Dirgantara <agungmasda29@gmail.com>
2018-11-22 14:08:43 +0700

    add stock module

---
+++ application/modules/stock/controllers/Stock.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+class Stock extends Rest_api
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/* List Stock */
+	public function index_get()
+	{
+		
+	}
+
+}
+
+/* End of file Stock.php */
+/* Location: ./application/modules/stock/controllers/Stock.php */
