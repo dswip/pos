1bf72eb07bd5d0b6de01802a34648634e5650bdc
Agung Dirgantara <agungmasda29@gmail.com>
2018-11-28 15:00:59 +0700

    update customer

--- application/modules/customer/controllers/Customer.php
+++ application/modules/customer/controllers/Customer.php
@@ -115,7 +115,70 @@ class Customer extends Rest_api
 	/* Update */
 	public function update_post()
 	{
-		
+		$this->form_validation->set_rules('member_id', 'member id', 'trim|required|integer');
+		$this->form_validation->set_rules('first_name', 'first name', 'trim|required');
+		$this->form_validation->set_rules('email', 'email', 'trim|required|valid_email');
+		$this->form_validation->set_rules('type','trim|required|in_list[customer,member]');
+		$this->form_validation->set_rules('address', 'address', 'trim|required');
+		$this->form_validation->set_rules('shipping_address', 'shipping_address', 'trim|required');
+		$this->form_validation->set_rules('phone1', 'phone1', 'trim|required');
+		$this->form_validation->set_rules('phone2', 'phone2', 'trim|required');
+		$this->form_validation->set_rules('fax', 'fax', 'trim|required');
+		$this->form_validation->set_rules('password', 'password', 'trim|required');
+		$this->form_validation->set_rules('website', 'website', 'trim|required');
+		$this->form_validation->set_rules('state', 'state', 'trim|required');
+		$this->form_validation->set_rules('city', 'city', 'trim|required');
+		$this->form_validation->set_rules('region', 'region', 'trim|required');
+		$this->form_validation->set_rules('zip', 'zip', 'trim|required');
+		$this->form_validation->set_rules('notes', 'notes', 'trim|required');
+		$this->form_validation->set_rules('status', 'status', 'trim|required|integer');
+		$this->form_validation->set_data($this->post());
+		if($this->form_validation->run() == TRUE)
+		{
+			$find = $this->customer->find($id);
+			if(!empty($find))
+			{
+				$find->member_id = return_if_exists($this->post(),'member_id',$find->member_id);
+				$find->first_name = return_if_exists($this->post(),'first_name',$find->first_name);
+				$find->last_name = return_if_exists($this->post(),'last_name',$find->last_name);
+				$find->type = return_if_exists($this->post(),'type',$find->type);
+				$find->address = return_if_exists($this->post(),'address',$find->address);
+				$find->shipping_address = return_if_exists($this->post(),'shipping_address',$find->shipping_address);
+				$find->phone1 = return_if_exists($this->post(),'phone1',$find->phone1);
+				$find->phone2 = return_if_exists($this->post(),'phone2',$find->phone2);
+				$find->fax = return_if_exists($this->post(),'fax',$find->fax);
+				$find->email = return_if_exists($this->post(),'email',$find->email);
+				$find->password = return_if_exists($this->post(),'password',$find->password);
+				$find->website = return_if_exists($this->post(),'website',$find->website);
+				$find->state = return_if_exists($this->post(),'state',$find->state);
+				$find->city = return_if_exists($this->post(),'city',$find->city);
+				$find->region = return_if_exists($this->post(),'region,$find->region');
+				$find->zip = return_if_exists($this->post(),'zip',$find->zip);
+				$find->notes = return_if_exists($this->post(),'notes',$find->notes);
+				$find->image = return_if_exists($this->post(),'image',$find->image);
+				$find->joined = return_if_exists($this->post(),'joined',$find->joined);
+				$find->status = return_if_exists($this->post(),'status',$find->status);
+				$find->save();
+				$response = 
+				[
+					'status' => 'success',
+					'data' => $data
+				];
+			}
+		}
+		else
+		{
+			$validation_errors = explode('<p>',str_replace('</p>','',validation_errors()));
+			array_shift($validation_errors);
+			$response = 
+			[
+				'status' => 'failed',
+				'message_code' => 'validation_error',
+				'message' => site_language('validation_error','validation error'),
+				'data' => $validation_errors
+			];
+		}
+		$this->response($response,REST_Controller::HTTP_OK);	
 	}
 
 	/* Delete */
